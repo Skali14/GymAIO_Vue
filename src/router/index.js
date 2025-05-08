8ce6507aadcd50232bbe7897a3805e5ff9b97a8d
Simon Kadlec <simon.kadlec@outlook.de>
2025-05-08 22:18:16 +0200

    prepared template

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ExerciseView from '@/views/ExerciseView.vue'
+import CalorieView from '@/views/CalorieView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/calorietracker',
+      name: 'calorietracker',
+      component: CalorieView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/exercises',
+      name: 'exercises',
+      component: ExerciseView,
     },
   ],
 })
