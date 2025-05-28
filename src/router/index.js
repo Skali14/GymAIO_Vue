cd0b1e5a5b83bc3fb35bef73bda82c179f4bf4ab
Michi <89996559+wtseller@users.noreply.github.com>
2025-05-28 20:14:33 +0200

    added login page skeletton

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ExerciseView from '@/views/ExerciseView.vue'
 import CalorieView from '@/views/CalorieView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,6 +20,11 @@ const router = createRouter({
       path: '/',
       name: 'exercises2',
       component: ExerciseView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
   ],
 })
